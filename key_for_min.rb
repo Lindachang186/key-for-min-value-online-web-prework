c0d7fa7cefde41530b282b497e1658b490ec4d35
Linda Chang <lindachang186@gmail.com>
2018-10-17 20:50:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
   name_hash.each do |key, value|
-    if value < min_value
-      puts min_value
+    if value < min_value && min_value != nil
+       min_value = value
     end
     return min_value
 end
